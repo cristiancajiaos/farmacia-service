25dfac1af3d3939f4c567af91b3316217f8a41d1
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-29 18:44:30 -0400

    Primer commit

---
+++ src/test/java/com/example/farmaciaservice/FarmaciaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmaciaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
